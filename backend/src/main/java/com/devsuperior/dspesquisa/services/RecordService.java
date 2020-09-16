cfbc106cf7329bf72410398148a6f40d7f00b593
Erasmo Ribeiro Bezerra <erasmo.ads.tech@gmail.com>
2020-09-16 12:10:02 -0300

    endpoint post/records

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dspesquisa.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dspesquisa.dto.RecordDTO;
+import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
+import com.devsuperior.dspesquisa.entities.Game;
+import com.devsuperior.dspesquisa.entities.Record;
+import com.devsuperior.dspesquisa.repositories.GameRepository;
+import com.devsuperior.dspesquisa.repositories.RecordRepository;
+
+
+// UM SERVIÇO PARA TRABALHAR COM GAMES. ESTE SIM PODE TER UM DEPENDÊNCIA PARA O GAME REPOSITORY
+
+@Service // PERMITE QUE O GameService POSSA SER INJETADO EM OUTRAS CLASSES
+public class RecordService {
+	
+	@Autowired // INJETA A DEPENDÊNCIA AUTOMATICAMENTE
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		
+		Record entity = new Record();
+		
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = repository.save(entity);
+		return new RecordDTO(entity);
+		
+		
+	}
+
+}
